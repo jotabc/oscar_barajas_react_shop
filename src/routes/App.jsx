29b7ac2237673adaf7a33360a3d78be16459e61e
jonnathan <jonnabcl56@gmail.com>
2023-02-20 18:39:52 -0500

    Atomic Design routes and section main

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Layout } from '../containers/Layout'
 import { Login } from '../containers/Login'
-import { RecoveryPassword } from '../containers/RecoveryPassword'
 import { Home } from '../pages/Home'
+import { PasswordRecovery } from '../pages/PasswordRecovery'
+import { SendEmail } from '../pages/SendEmail'
 import { NotFound } from '../pages/NotFound'
+import { NewPassword } from '../pages/NewPassword'
+import { MyAccount } from '../pages/MyAccount'
+import { Checkout } from '../pages/Checkout'
+import { Orders } from '../pages/Orders'
 import '../styles/global.css'
 
 export const App = () => {
@@ -14,7 +19,12 @@ export const App = () => {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/login' component={Login} />
-          <Route exact path='/recovery_password' component={RecoveryPassword} />
+          <Route exact path='/password_recovery' component={PasswordRecovery} />
+          <Route exact path='/send_email' component={SendEmail} />
+          <Route exact path='/new_password' component={NewPassword} />
+          <Route exact path='/account' component={MyAccount} />
+          <Route exact path='/checkout' component={Checkout} />
+          <Route exact path='/orders' component={Orders} />
           <Route path='*' component={NotFound} />
         </Switch>
       </Layout>
