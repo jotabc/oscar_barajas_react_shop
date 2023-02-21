9271804171fba9796abc9ce7f6e4017fdb87ac30
jonnathan <jonnabcl56@gmail.com>
2023-02-20 19:52:52 -0500

    images + webpack config for images + alias in webpack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
-import '../styles/Header.scss'
+import '@styles/Header.scss'
+import menu from '@icons/icon_menu.svg'
+import logo from '@logos/logo_yard_sale.svg'
+import shoppingCart from '@icons/icon_shopping_cart.svg'
 
 export const Header = () => {
   return (
     <nav>
       <img
-        src='./icons/icon_menu.svg'
+        src={menu}
         alt='menu'
         className='menu'
       />
       <div className='navbar-left'>
         <img
-          src='./logos/logo_yard_sale.svg'
+          src={logo}
           alt='logo'
-          className='logo'
+          className='nav-logo'
         />
         <ul>
           <li><a href='/'>All</a></li>
@@ -29,7 +32,7 @@ export const Header = () => {
           <li className='navbar-email'>platzi@example.com</li>
           <li className='navbar-shopping-cart'>
             <img
-              src='./icons/icon_shopping_cart.svg'
+              src={shoppingCart}
               alt='shopping cart'
             />
             <div>2</div>
