cbdad88d45d40ab19f9dda7ac5b964b5525b926d
jonnathan <jonnabcl56@gmail.com>
2023-02-21 12:45:30 -0500

    ReactContext ( Total price)

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -2,14 +2,22 @@ import React, { useContext } from 'react'
 import { OrderItem } from '../components/OrderItem'
 import AppContext from '../context/AppContext'
 import '../styles/MyOrder.scss'
+import arrowLeft from '@icons/flechita.svg'
 
 export const MyOrder = () => {
   const { state } = useContext(AppContext)
 
+  const sumTotal = () => {
+    const reducer = (acc, currentValue) => acc + currentValue.price
+
+    const sum = state.cart.reduce(reducer, 0)
+    return sum
+  }
+
   return (
     <aside className='MyOrder'>
       <div className='title-container'>
-        <img src='./icons/flechita.svg' alt='arrow' />
+        <img src={arrowLeft} alt='arrow Left' />
         <p className='title'>My order</p>
       </div>
       <div className='my-order-content'>
@@ -19,12 +27,11 @@ export const MyOrder = () => {
             product={productCart}
           />
         ))}
-        <OrderItem />
         <div className='order'>
           <p>
             <span>Total</span>
           </p>
-          <p>$560.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button className='primary-button'>
           Checkout
