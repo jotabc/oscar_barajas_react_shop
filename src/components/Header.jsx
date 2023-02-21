ed28d401c34a7b41fceac07f7512f3829313dc71
jonnathan <jonnabcl56@gmail.com>
2023-02-20 20:16:08 -0500

    toggle menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '@styles/Header.scss'
 import menu from '@icons/icon_menu.svg'
 import logo from '@logos/logo_yard_sale.svg'
 import shoppingCart from '@icons/icon_shopping_cart.svg'
+import { Menu } from '@components/Menu'
 
 export const Header = () => {
+  const [toggle, setToggle] = useState(false)
+
+  const handleToggle = () => {
+    setToggle(!toggle)
+  }
+
   return (
     <nav>
       <img
@@ -29,7 +36,9 @@ export const Header = () => {
       </div>
       <div className='navbar-right'>
         <ul>
-          <li className='navbar-email'>platzi@example.com</li>
+          <li className='navbar-email' onClick={handleToggle}>
+            platzi@example.com
+          </li>
           <li className='navbar-shopping-cart'>
             <img
               src={shoppingCart}
@@ -39,6 +48,7 @@ export const Header = () => {
           </li>
         </ul>
       </div>
+      {toggle && <Menu />}
     </nav>
   )
 }
